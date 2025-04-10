45d53ca223db541aa75fdaffb955bdfac4bf1648
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2025-04-11 00:44:18 +0700

    feat: menambahkan fitur edit tugas

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function addTodo() {
   if (input.value !== "") {
     const li = document.createElement("li");
     li.textContent = input.value;
+
+    li.addEventListener("dblclick", () => {
+      const newTask = prompt("Edit tugas:", li.textContent);
+      if (newTask) li.textContent = newTask;
+    });
+
     list.appendChild(li);
     input.value = "";
   }
